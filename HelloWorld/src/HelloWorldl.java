bf98aa3993e58dd127de4fa4ad5462811577bbc5
hkon123 <hkonhd@gmail.com>
2020-11-02 11:45:15 +0100

    New Test proj, HelloWorld
    This is a test

---
+++ HelloWorld/src/HelloWorldl.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author Håkon Dølving
+ *
+ */
+public class HelloWorldl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2;
+		
+		
+		System.out.println("Hello world!");
+		
+		num1=1;
+		num2=num1 + 1;
+		
+		int num3=num1 + num2;
+		
+		System.out.printf("%d, %d, %d",num1, num2, num3);
+		
+	}
+
+}
